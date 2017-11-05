c88daae976c0431faea7fb2aacd494cc7331e192
lin-brian-l <lin.brian.l@gmail.com>
2017-11-05 10:56:03 -0600

    Write constructor function for Board class.

--- cell.js
+++ cell.js
@@ -1,17 +1,17 @@
 class Cell {
 	constructor(row, column, grid, value) {
-		this.row = row
-		this.column = column
-		this.grid = grid
-		this.values = this.checkValue(value)
-	}
+		this.row = row;
+		this.column = column;
+		this.grid = grid;
+		this.values = this.checkValue(value);
+	};
 
 	checkValue(value) {
 		if (value === "-") {
-			return [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		}
-		return [value]
-	}
-}
+			return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		};
+		return [value];
+	};
+};
 
-module.exports = Cell 
+module.exports = Cell;
