698499b826d10ba294b15ee23b68471cf96f9dc5
Brian Lin <lin.brian.l@gmail.com>
2018-07-24 09:25:48 -0500

    Install yarn on windows and slightly refactor cell.js.

--- cell.js
+++ cell.js
@@ -9,9 +9,8 @@ class Cell {
 	checkValue(value) {
 		if (value === "-") {
 			return [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		} else {
-			return [parseInt(value)];
-		}
+		};
+		return [parseInt(value)];
 	};
 };
 
