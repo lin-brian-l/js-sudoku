58868974ae96f4f179204fa96f6c1975096f4e7c
Brian Lin <lin.brian.l@gmail.com>
2018-07-25 00:37:13 -0500

    Clean up bolded class functions for legibility.

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ $(document).ready(() => {
 let createBoard = () => {
 	for (var cell = 0; cell < 81; cell++) {
 		let divClass = 'cell';
-		if ((cell + 1) % 3 === 0) divClass += ' right-bold';
-		if (Math.ceil((cell + 1) / 9 ) % 3 === 0) divClass += ' bottom-bold';
-		$(".board").append(`<div class='${divClass}'></div>`);	
+		if (rightBolded(cell)) divClass += ' right-bold';
+		if (bottomBolded(cell)) divClass += ' bottom-bold';
+		$(".board").append(`<div id='${cell}' class='${divClass}'></div>`);	
 	}
+}
+
+let rightBolded = (cell) => {
+	return (cell + 1) % 3 === 0 && (cell + 1) % 9 !== 0;
+}
+
+let bottomBolded = (cell) => {
+	return Math.ceil((cell + 1) / 9 ) % 3 === 0 && Math.ceil((cell + 1) / 9 ) < 9;
 }
