f7bd020f333225e63d0d456a8789110e7a60512b
Brian Lin <lin.brian.l@gmail.com>
2018-07-25 01:11:45 -0500

    Attempt to add input elements, but cannot get proper styling.

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ $(document).ready(() => {
 
 let createBoard = () => {
 	for (var cell = 0; cell < 81; cell++) {
-		let divClass = 'cell';
-		if (rightBolded(cell)) divClass += ' right-bold';
-		if (bottomBolded(cell)) divClass += ' bottom-bold';
-		$(".board").append(`<div id='${cell}' class='${divClass}'></div>`);	
+		let inputClass = 'cell';
+		if (rightBolded(cell)) inputClass += ' right-bold';
+		if (bottomBolded(cell)) inputClass += ' bottom-bold';
+		$(".board").append(`<input id='${cell}' type='text' class='${inputClass}'></input>`);	
+
+		// let divClass = 'cell';
+		// if (rightBolded(cell)) divClass += ' right-bold';
+		// if (bottomBolded(cell)) divClass += ' bottom-bold';
+		// $(".board").append(`<div id='${cell}' class='${divClass}'></div>`);	
 	}
 }
 
