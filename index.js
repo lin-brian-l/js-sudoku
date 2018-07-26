923bbffee5cbd3823d9ef90fa622fc3bd001b17f
Brian Lin <lin.brian.l@gmail.com>
2018-07-25 20:52:28 -0500

    Style font in input, add and position button, and write function to create boardstring.

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 $(document).ready(() => {
 	createBoard();
 
-	$('input').on('')
+	$('input').keypress(validateInput);
+
+	$('button').click(createBoardString);
 })
+
+let validateInput = (event) => {
+	let text = String.fromCharCode(event.which);
+	return /[1-9]/.test(text);	
+}
 
 let createBoard = () => {
 	for (var cell = 0; cell < 81; cell++) {
 		let divClass = 'cell';
-		if (rightBolded(cell)) divClass += ' right-bold';
-		if (bottomBolded(cell)) divClass += ' bottom-bold';
+		if (rightBoldedCheck(cell)) divClass += ' right-bold';
+		if (bottomBoldedCheck(cell)) divClass += ' bottom-bold';
 		let inputElement = `<input type='text' name='input${cell}' maxlength='1'></input>`;
 		let divElement = `<div id='${cell}' class='${divClass}'>${inputElement}</div>`;
 		$(".board").append(divElement);	
 	}
 }
 
-let rightBolded = (cell) => {
+let rightBoldedCheck = (cell) => {
 	return (cell + 1) % 3 === 0 && (cell + 1) % 9 !== 0;
 }
 
-let bottomBolded = (cell) => {
+let bottomBoldedCheck = (cell) => {
 	return Math.ceil((cell + 1) / 9 ) % 3 === 0 && Math.ceil((cell + 1) / 9 ) < 9;
+}
+
+let createBoardString = (event) => {
+	let boardString = "";
+	$('input').each((index, input) => {
+		boardString += $(input).val() || "-";
+	});
+	console.log("boardString: " + boardString);
 }
